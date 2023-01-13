9ccce3b5e5d490a1fa66f7c46763f0a8187c8261
agiang8397 <agiang8397@gmail.com>
2023-01-13 13:30:35 +0700

    4

--- src/components/BasicCard.js
+++ src/components/BasicCard.js
@@ -4,7 +4,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Box } from "@mui/system";
 import { Chip } from "@mui/material";
 
